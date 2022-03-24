3a1b8f9e761f078321a58777a81ea3514e23b118
Uma Durairaj <uduraira@usc.edu>
2022-03-23 18:08:25 -0700

    slideshow

--- app/src/main/java/com/example/easyteamup/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/easyteamup/ui/slideshow/SlideshowFragment.java
@@ -8,30 +8,25 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+
+import com.example.easyteamup.databinding.FragmentSlideshowBinding;
 
 
 public class SlideshowFragment extends Fragment {
 
-//    private FragmentSlideshowBinding binding;
+    private FragmentSlideshowBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        SlideshowViewModel slideshowViewModel =
-//                new ViewModelProvider(this).get(SlideshowViewModel.class);
 
-//        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-//        View root = binding.getRoot();
-//
-//        final TextView textView = binding.textSlideshow;
-//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//        return root;
-        return null;
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-//        binding = null;
+        binding = null;
     }
 }
