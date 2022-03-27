361190078353c187f907a3810c6365e1f5317ad0
jojamu7 <jmulholl@usc.edu>
2022-03-26 23:32:10 -0700

    list of events will work ONCE PROPER DATA RECEIVED FROM DATABASE

--- app/src/main/java/com/example/easyteamup/ui/shared/ListFragment.java
+++ app/src/main/java/com/example/easyteamup/ui/shared/ListFragment.java
@@ -1,31 +1,50 @@
 package com.example.easyteamup.ui.shared;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import com.example.easyteamup.EventTable;
 import com.example.easyteamup.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
 
 public class ListFragment extends Fragment
 {
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // init view
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
 
+
         return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        EventTable et = new EventTable();
+        ListView list = getView().findViewById(R.id.listOfEvents);
+        String[] eventArr = new String[et.size()];
+        eventArr = et.getAllEventNames();
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, eventArr);
+        list.setAdapter(adapter);
+    }
+
 
 }
