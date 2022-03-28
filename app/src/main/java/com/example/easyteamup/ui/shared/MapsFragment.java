a9ffb71610daca36a421de1cbede9f6cfec3afd2
jojamu7 <jmulholl@usc.edu>
2022-03-27 19:40:20 -0700

    map markers updating

--- app/src/main/java/com/example/easyteamup/ui/shared/MapsFragment.java
+++ app/src/main/java/com/example/easyteamup/ui/shared/MapsFragment.java
@@ -11,11 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import com.example.easyteamup.Event;
 import com.example.easyteamup.EventTable;
 import com.example.easyteamup.Location;
+import com.example.easyteamup.OnIntegerChangeListener;
 import com.example.easyteamup.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -38,50 +40,51 @@ public class MapsFragment extends Fragment{
         // init map fragment
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
         // async map
+        EventTable et = new EventTable();
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
 
-                // when map is loaded
-//                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-//                    @Override
-//                    public void onMapClick(@NonNull LatLng latLng) {
-//                        // when clicked on map, init marker options
-//                        MarkerOptions markerOptions = new MarkerOptions();
-//                        // set pos of marker
-//                        markerOptions.position(latLng);
-//                        // set title of marker
-//                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-//                        // remove all markers
-//                        googleMap.clear();
-//                        // animating to zoom the marker
-//                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-//                        // add marker on map
-//                        googleMap.addMarker(markerOptions);
-//                    }
-//                });
+                ArrayList<Event> allEvents;
+                allEvents = et.getAllEvents();
+                for (int i = 0; i < allEvents.size(); i++)
+                {
+                    Location loc = allEvents.get(i).getLocation();
+                    String title = allEvents.get(i).getEventName();
+                    double longitude = loc.getLongitude();
+                    double latitude = loc.getLatitude();
+                    googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(title));
+                }
+            }
+        });
+        et.setOnIntegerChangeListener(new OnIntegerChangeListener() {
+            @Override
+            public void onIntegerChanged(int newValue) {
+                System.out.println("new value " + newValue);
                 EventTable et = new EventTable();
                 ArrayList<Event> allEvents;
                 allEvents = et.getAllEvents();
-//                for (int i = 0; i < allEvents.size(); i++)
-//                {
-//                    Location loc = allEvents.get(i).getLocation();
-//                    String title = allEvents.get(i).getEventName();
-//                    double longitude = loc.getLongitude();
-//                    double latitude = loc.getLatitude();
-//                    googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(title));
-//                }
+                System.out.println(et.size());
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+
+                supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
+                        EventTable et = new EventTable();
+                        ArrayList<Event> allEvents;
+                        allEvents = et.getAllEvents();
+                        for (int i = 0; i < allEvents.size(); i++)
+                        {
+                            Location loc = allEvents.get(i).getLocation();
+                            String title = allEvents.get(i).getEventName();
+                            double longitude = loc.getLongitude();
+                            double latitude = loc.getLatitude();
+                            googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(title));
+                        }
+                    }
+                });
             }
         });
-
         return view;
     }
-
-    // Get a handle to the GoogleMap object and display marker.
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        googleMap.addMarker(new MarkerOptions()
-//                .position(new LatLng(0, 0))
-//                .title("Marker"));
-//    }
 }
