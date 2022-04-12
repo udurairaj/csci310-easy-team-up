929d058010de16e64e08657149253666eba7699f
Uma Durairaj <uduraira@usc.edu>
2022-04-11 22:27:39 -0700

    black box testing set up

--- app/src/androidTest/java/com/example/easyteamup/BasicFeatureBlackBoxTests.java
+++ app/src/androidTest/java/com/example/easyteamup/BasicFeatureBlackBoxTests.java
@@ -8,9 +8,19 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.example.easyteamup.login.LoginActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 public class BasicFeatureBlackBoxTests {
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> loginRule =
+            new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
+
     @Test
     public void testLogin() {
         onView(withId(R.id.usernameLoginBox)).perform(typeText("bob"));
