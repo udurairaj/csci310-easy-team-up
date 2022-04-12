a673a9135b4efec14950a21a0ceba82c569bf92f
Uma Durairaj <uduraira@usc.edu>
2022-04-11 23:41:29 -0700

    nav black box

--- app/src/androidTest/java/com/example/easyteamup/BasicFeatureBlackBoxTests.java
+++ app/src/androidTest/java/com/example/easyteamup/BasicFeatureBlackBoxTests.java
@@ -3,18 +3,24 @@ package com.example.easyteamup;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.easyteamup.login.LoginActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class BasicFeatureBlackBoxTests {
 
     @Rule
@@ -27,5 +33,9 @@ public class BasicFeatureBlackBoxTests {
         onView(withId(R.id.passwordLoginBox)).perform(typeText("bobbobbob"), closeSoftKeyboard());
         onView(withId(R.id.loginButton)).perform(click());
         onView(withId(R.id.nameProfileView)).check(matches(withText("billy bobby :)")));
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+
+        onView(withId(R.id.nav_userEventDisplay)).perform(click());
     }
 }
