60eea390cb6c46bf3a189594fae5d61c82da0f85
Uma Durairaj <uduraira@usc.edu>
2022-04-13 03:03:54 -0700

    tests

--- app/src/test/java/com/example/easyteamup/Feature3WhiteBoxTests.java
+++ app/src/test/java/com/example/easyteamup/Feature3WhiteBoxTests.java
@@ -9,10 +9,5 @@ import org.mockito.Mockito;
 import org.mockito.Mockito.*;
 
 public class Feature3WhiteBoxTests {
-    @Test
-    public void testAddEvent() {
-        EventTable table = new EventTable(true);
-        Event event = new Event(999, "My Event", true);
-       Assert.assertNotEquals(table.getEvent(table.addEvent(event, true)), null);
-    }
+
 }
